a28c7559998b38092415d4ac0bc5be58b50e1257
Jason <jsbullara@gmail.com>
2016-11-16 16:17:49 -0500

    remove firebase for now

--- app/src/main/java/net/bigblue/bb/blueapp/firebaseMessage.java
+++ app/src/main/java/net/bigblue/bb/blueapp/firebaseMessage.java
@@ -7,9 +7,9 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
  * Created by jason on 11/8/16.
  */
 
-public class firebaseMessage extends FirebaseInstanceIdService {
+public class firebaseMessage  {
 
-   @Override
+  // @Override extends FirebaseInstanceIdService
     public void onTokenRefresh(){
        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
 
